41b8359a63ee53c66f2ba119138150e3e693d85c
RaoMujtaba <mujtabarao99@gmail.com>
2024-03-13 14:09:18 +0500

    Initial commit

---
+++ app/src/main/java/com/example/videoapp/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.videoapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private SurfaceView surfaceView;
+    private MediaPlayer mediaplayer;
+    private SeekBar seekbar;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        seekbar = findViewById(R.id.seekBar);
+        button = findViewById(R.id.button);
+        surfaceView = findViewById(R.id.surfaceView);
+        mediaplayer = MediaPlayer.create(this,R.raw.media);
+        surfaceView.setKeepScreenOn(true);
+        SurfaceHolder surfaceHolder = surfaceView.getHolder();
+        surfaceHolder.addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder) {
+                mediaplayer.setDisplay(surfaceHolder);
+            }
+
+            @Override
+            public void surfaceChanged(@NonNull SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(@NonNull SurfaceHolder surfaceHolder) {
+
+            }
+        });
+
+
+        seekbar.setMax(mediaplayer.getDuration());
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                if(b)
+                {
+                    mediaplayer.seekTo(i);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mediaplayer.isPlaying())
+                {
+                    mediaplayer.pause();
+                    button.setText("Play");
+                }
+                else
+                {
+                    mediaplayer.start();
+                    button.setText("Pause");
+                }
+            }
+        });
+    }
+}
